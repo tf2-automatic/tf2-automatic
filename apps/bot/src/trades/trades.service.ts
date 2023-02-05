0ab70c059ad5d3a5b96fbbf865367613b38e5cac
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2023-02-05 23:49:16 +0100

    chore(bot): add logging to trades

--- apps/bot/src/trades/trades.service.ts
+++ apps/bot/src/trades/trades.service.ts
@@ -15,9 +15,12 @@ import {
 import { EResultException } from '../common/exceptions/eresult.exception';
 import { Config, SteamAccountConfig } from '../common/config/configuration';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common/services';
 
 @Injectable()
 export class TradesService {
+  private readonly logger: Logger = new Logger(TradesService.name);
+
   private readonly manager = this.botService.getManager();
   private readonly community = this.botService.getCommunity();
 
@@ -83,8 +86,28 @@ export class TradesService {
       offer.addMyItems(dto.itemsToGive);
       offer.addTheirItems(dto.itemsToReceive);
 
+      this.logger.log(`Sending trade offer to ${dto.partner}...`);
+
+      this.logger.debug(
+        `Items to give: [${dto.itemsToGive
+          .map((item) => `"${item.appid}_${item.contextid}_${item.assetid}"`)
+          .join(',')}]`
+      );
+      this.logger.debug(
+        `Items to receive: [${dto.itemsToReceive
+          .map((item) => `"${item.appid}_${item.contextid}_${item.assetid}"`)
+          .join(',')}]`
+      );
+
       offer.send((err) => {
         if (err) {
+          this.logger.error(
+            `Got an error while sending trade offer: ${err.message}${
+              err.eresult !== undefined ? ` (eresult: ${err.eresult})` : ''
+            }`
+          );
+          this.logger.debug(err);
+
           if (err.message === 'Cannot send an empty trade offer') {
             return reject(
               new BadRequestException('Cannot send an empty trade offer')
@@ -98,6 +121,12 @@ export class TradesService {
           return reject(err);
         }
 
+        this.logger.log(
+          `Sent trade offer #${offer.id} to ${dto.partner} has status ${
+            SteamTradeOfferManager.ETradeOfferState[offer.state]
+          }`
+        );
+
         return resolve(this.mapOffer(offer));
       });
     });
@@ -105,12 +134,18 @@ export class TradesService {
 
   acceptConfirmation(id: string): Promise<void> {
     return new Promise((resolve, reject) => {
+      this.logger.log(`Accepting confirmation for offer #${id}...`);
+
       this.community.acceptConfirmationForObject(
         this.configService.getOrThrow<SteamAccountConfig>('steam')
           .identitySecret,
         id,
         (err) => {
           if (err) {
+            this.logger.error(
+              `Error while accepting confirmation for ${id}: ` + err.message
+            );
+            this.logger.debug(err);
             if (
               err.message ===
               'Could not find confirmation for object ' + id
@@ -121,6 +156,8 @@ export class TradesService {
             return reject(err);
           }
 
+          this.logger.log(`Accepted confirmation for offer #${id}`);
+
           return resolve();
         }
       );
@@ -129,8 +166,14 @@ export class TradesService {
 
   removeTrade(id: string): Promise<TradeOffer> {
     return new Promise((resolve, reject) => {
+      this.logger.log('Canceling/declining trade offer #' + id + '...');
+
       this.manager.getOffer(id, (err, offer) => {
         if (err) {
+          this.logger.error(
+            'Error while getting trade offer #' + id + ': ' + err.message
+          );
+          this.logger.debug(err);
           if (err.message === 'NoMatch') {
             return reject(new BadRequestException('Trade offer not found'));
           }
@@ -140,12 +183,21 @@ export class TradesService {
 
         offer.cancel((err) => {
           if (err) {
+            this.logger.error(
+              'Error while canceling/declining trade offer #' +
+                id +
+                ': ' +
+                err.message
+            );
+            this.logger.debug(err);
             if (err.eresult !== undefined) {
               return reject(new EResultException(err.message, err.eresult));
             }
 
             return reject(err);
           }
+
+          this.logger.log('Canceled/declined trade offer #' + id);
 
           return resolve(this.mapOffer(offer));
         });
