02bdfd8dd39ec1c84ca09e2555c739582b9d1dd7
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2023-03-02 23:01:17 +0100

    feat(bot): add request summary metric

--- apps/bot/src/bot/bot.module.ts
+++ apps/bot/src/bot/bot.module.ts
@@ -5,10 +5,18 @@ import { ShutdownModule } from '../shutdown/shutdown.module';
 import { StorageModule } from '../storage/storage.module';
 import { BotService } from './bot.service';
 import { BotController } from './bot.controller';
+import { makeSummaryProvider } from '@willsoto/nestjs-prometheus';
 
 @Module({
   imports: [StorageModule, EventsModule, MetadataModule, ShutdownModule],
-  providers: [BotService],
+  providers: [
+    BotService,
+    makeSummaryProvider({
+      name: 'steam_api_request_duration_seconds',
+      help: 'The duration of Steam API requests in seconds',
+      labelNames: ['method', 'url', 'status'],
+    }),
+  ],
   controllers: [BotController],
   exports: [BotService],
 })
