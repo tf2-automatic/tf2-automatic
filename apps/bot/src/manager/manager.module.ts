4b9fac5323238c041f83cf3705441f6afd8bded7
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2023-03-25 15:35:43 +0100

    fix(bot): improve delete bot on shutdown

--- apps/bot/src/manager/manager.module.ts
+++ apps/bot/src/manager/manager.module.ts
@@ -1,10 +1,10 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { BotModule } from '../bot/bot.module';
+import { MetadataModule } from '../metadata/metadata.module';
 import { ManagerService } from './manager.service';
 
 @Module({
-  imports: [HttpModule, BotModule],
+  imports: [HttpModule, MetadataModule],
   providers: [ManagerService],
 })
 export class ManagerModule {}
