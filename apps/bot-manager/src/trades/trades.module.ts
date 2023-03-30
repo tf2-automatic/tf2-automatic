dab0d6c00ab568c3569b8d6b8f265a734bdcbe8c
Nicklas <nicklas.marc.pedersen@gmail.com>
2023-03-30 14:00:42 +0200

    feat(bot-manager): trade queue (#164)
    With the trade queue, you can create, accept, decline / cancel, and
    confirm offers reliably. If a job encounters an error, such as the bot
    not being available, or Steam gives errors, then the job will be
    retried.
    
    Retries can be configured using the following options:
    * `strategy` - `"exponential"`, `"linear"` or `"fixed"`.
    * `maxTime` - Maximum job age in milliseconds before being deleted.
    * `delay` - Delay factor in milliseconds for the different strategies.
    * `maxDelay` - Maximum delay between each retry.
    
    Jobs can also be prioritized using `priority`. The highest priority is
    1.

--- apps/bot-manager/src/trades/trades.module.ts
+++ apps/bot-manager/src/trades/trades.module.ts
@@ -4,8 +4,20 @@ import { Module } from '@nestjs/common';
 import { EventsModule } from '../events/events.module';
 import { HeartbeatsModule } from '../heartbeats/heartbeats.module';
 import { RabbitMQWrapperModule } from '../rabbitmq-wrapper/rabbitmq-wrapper.module';
-import { ExchangeDetailsProcessor } from './exchange-details.processor';
+import { ExchangeDetailsProcessor } from './processors/exchange-details.processor';
 import { TradesService } from './trades.service';
+import { TradesController } from './trades.controller';
+import { DefaultJobOptions } from 'bullmq';
+import { TradesProcessor } from './processors/trades.processor';
+
+const defaultJobOptions: DefaultJobOptions = {
+  attempts: Number.MAX_SAFE_INTEGER,
+  backoff: {
+    type: 'custom',
+  },
+  removeOnComplete: true,
+  removeOnFail: true,
+};
 
 @Module({
   imports: [
@@ -13,19 +25,16 @@ import { TradesService } from './trades.service';
     RabbitMQWrapperModule,
     BullModule.registerQueue({
       name: 'getExchangeDetails',
-      defaultJobOptions: {
-        attempts: Number.MAX_SAFE_INTEGER,
-        backoff: {
-          type: 'exponential',
-          delay: 10000,
-        },
-        removeOnComplete: true,
-        removeOnFail: true,
-      },
+      defaultJobOptions,
+    }),
+    BullModule.registerQueue({
+      name: 'trades',
+      defaultJobOptions,
     }),
     HeartbeatsModule,
     HttpModule,
   ],
-  providers: [TradesService, ExchangeDetailsProcessor],
+  providers: [TradesService, ExchangeDetailsProcessor, TradesProcessor],
+  controllers: [TradesController],
 })
 export class TradesModule {}
--- libs/bot-manager-data/src/lib/trades.ts
+++ libs/bot-manager-data/src/lib/trades.ts
@@ -1,8 +1,48 @@
 import {
   BaseEvent,
+  CreateTrade,
   TradeOffer,
   TradeOfferExchangeDetails,
 } from '@tf2-automatic/bot-data';
+
+export const TRADES_BASE_URL = '/trades';
+export const TRADE_JOBS_PATH = `/`;
+export const TRADE_JOB_PATH = `/:id`;
+
+export interface RetryTradeOptions {
+  strategy?: 'exponential' | 'linear' | 'fixed';
+  maxTime?: number;
+  delay?: number;
+  maxDelay?: number;
+}
+
+export const QueueTradeTypes = [
+  'CREATE',
+  'DELETE',
+  'ACCEPT',
+  'CONFIRM',
+] as const;
+export type QueueTradeType = (typeof QueueTradeTypes)[number];
+
+export type QueueTradeCreate = QueueTrade<'CREATE', CreateTrade>;
+export type QueueTradeDelete = QueueTrade<'DELETE', string>;
+export type QueueTradeAccept = QueueTrade<'ACCEPT', string>;
+export type QueueTradeConfirm = QueueTrade<'CONFIRM', string>;
+
+export interface QueueTrade<
+  Type extends QueueTradeType = QueueTradeType,
+  Data = unknown
+> {
+  type: Type;
+  data: Data;
+  bot: string;
+  priority?: number;
+  retry?: RetryTradeOptions;
+}
+
+export interface QueueTradeResponse {
+  id: string;
+}
 
 export type ExchangeDetailsEventType = 'trades.exchange-details';
 
