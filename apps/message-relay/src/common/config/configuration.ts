b5a6ba052e82dcfbbe9c78650479f7035f4fcb11
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2023-06-22 21:23:40 +0200

    feat: message relay application
    New program used to implement the transactional outbox pattern

---
+++ apps/message-relay/src/common/config/configuration.ts
@@ -0,0 +1,44 @@
+export interface Config {
+  port: number;
+  redis: RedisConfig;
+  rabbitmq: RabbitMQConfig;
+}
+
+export interface RedisConfig {
+  host: string;
+  port: number;
+  password?: string;
+  db?: number;
+  keyPrefix?: string;
+}
+
+export interface RabbitMQConfig {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  vhost: string;
+}
+
+export default (): Config => {
+  return {
+    port: parseInt(process.env.PORT as string, 10),
+    rabbitmq: {
+      host: process.env.RABBITMQ_HOST as string,
+      port: parseInt(process.env.RABBITMQ_PORT as string, 10),
+      username: process.env.RABBITMQ_USERNAME as string,
+      password: process.env.RABBITMQ_PASSWORD as string,
+      vhost: process.env.RABBITMQ_VHOST as string,
+    },
+    redis: {
+      host: process.env.REDIS_HOST as string,
+      port: parseInt(process.env.REDIS_PORT as string, 10),
+      password: process.env.REDIS_PASSWORD,
+      db:
+        process.env.REDIS_DB !== undefined
+          ? parseInt(process.env.REDIS_DB, 10)
+          : undefined,
+      keyPrefix: process.env.REDIS_PREFIX ?? 'tf2-automatic',
+    },
+  };
+};
---
+++ apps/message-relay/src/main.ts
@@ -0,0 +1,21 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+import { Config } from './common/config/configuration';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService: ConfigService<Config> = app.get(ConfigService);
+
+  app.enableShutdownHooks();
+
+  const port = configService.getOrThrow<number>('port');
+
+  await app.listen(port);
+
+  Logger.log(`Application is running on: http://localhost:${port}/`);
+}
+
+bootstrap();
