f732ec48d03babbab73914a75f1409328f3dd9ca
Nicklas <nicklas.marc.pedersen@gmail.com>
2023-06-25 23:19:42 +0200

    feat(bot-manager): delete bot if no heartbeat is made (#252)
    Create job when heartbeat is made to check if a new heartbeat is made
    after the given interval
    
    Fix #240

--- apps/bot-manager/src/heartbeats/heartbeats.module.ts
+++ apps/bot-manager/src/heartbeats/heartbeats.module.ts
@@ -3,11 +3,28 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { HeartbeatsController } from './heartbeats.controller';
 import { HeartbeatsService } from './heartbeats.service';
+import { BullModule } from '@nestjs/bullmq';
+import { HeartbeatsProcessor } from './heartbeats.processor';
 
 @Module({
-  imports: [RedisModule, HttpModule],
+  imports: [
+    RedisModule,
+    HttpModule,
+    BullModule.registerQueue({
+      name: 'heartbeats',
+      defaultJobOptions: {
+        attempts: 3,
+        backoff: {
+          type: 'exponential',
+          delay: 1000,
+        },
+        removeOnComplete: true,
+        removeOnFail: true,
+      },
+    }),
+  ],
   controllers: [HeartbeatsController],
-  providers: [HeartbeatsService],
+  providers: [HeartbeatsService, HeartbeatsProcessor],
   exports: [HeartbeatsService],
 })
 export class HeartbeatsModule {}
