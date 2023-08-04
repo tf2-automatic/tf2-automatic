9ec8266324dd7bbe35bbfcb520c9b02b6ba89348
Nicklason <nicklas.marc.pedersen@gmail.com>
2023-08-04 18:21:30 +0200

    chore: add swagger to agents controller

--- apps/bptf-manager/src/agents/agents.controller.ts
+++ apps/bptf-manager/src/agents/agents.controller.ts
@@ -7,17 +7,28 @@ import {
   AGENT_REGISTER_PATH,
   AGENT_UNREGISTER_PATH,
 } from '@tf2-automatic/bptf-manager-data';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Agents')
 @Controller(AGENTS_BASE_URL)
 export class AgentsController {
   constructor(private readonly agentsService: AgentsService) {}
 
+  @ApiOperation({
+    summary: 'Register an agent',
+    description: 'Repeatedly registers an agent with backpack.tf',
+  })
   @Post(AGENT_REGISTER_PATH)
   @HttpCode(HttpStatus.OK)
   async registerAgent(@Param('steamid', ParseSteamIDPipe) steamid: SteamID) {
     await this.agentsService.enqueueRegisterAgent(steamid);
   }
 
+  @ApiOperation({
+    summary: 'Unregister an agent',
+    description:
+      'Stops repeatedly registering an agent and unregisters it with backpack.tf',
+  })
   @Post(AGENT_UNREGISTER_PATH)
   @HttpCode(HttpStatus.OK)
   async unregisterAgent(@Param('steamid', ParseSteamIDPipe) steamid: SteamID) {
