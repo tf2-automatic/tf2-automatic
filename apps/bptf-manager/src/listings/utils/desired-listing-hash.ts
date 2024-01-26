34693187b90f886a9518ae485ae844477d084278
Nicklason <Nicklason@users.noreply.github.com>
2024-01-26 14:22:17 +0100

    refactor(bptf-manager): fix types for hash listing method

--- apps/bptf-manager/src/listings/utils/desired-listing-hash.ts
+++ apps/bptf-manager/src/listings/utils/desired-listing-hash.ts
@@ -1,7 +1,12 @@
-import { ListingDto } from '@tf2-automatic/bptf-manager-data';
+import {
+  AddListingDto,
+  RemoveListingDto,
+} from '@tf2-automatic/bptf-manager-data';
 import hash from 'object-hash';
 
-export default function hashListing(listing: ListingDto): string {
+export default function hashListing(
+  listing: AddListingDto | RemoveListingDto,
+): string {
   if (listing.id) {
     return hash(listing.id);
   }
