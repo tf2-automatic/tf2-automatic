b4653ae67c4e41c0e43e36502a26c89f95d6c3f7
Zeus <zeusjunioryt@gmail.com>
2024-01-28 15:38:58 +0100

    feat(bot-data): shorten FULL_PATH exports a bit

--- libs/bot-data/src/lib/escrow.ts
+++ libs/bot-data/src/lib/escrow.ts
@@ -1,7 +1,7 @@
 export const ESCROW_BASE_URL = '/escrow';
 export const ESCROW_GET_DURATION = '/:steamid';
 // Full path for use when making HTTP requests
-export const ESCROW_DURATION_FULL_PATH = `${ESCROW_BASE_URL}${ESCROW_GET_DURATION}`;
+export const ESCROW_FULL_PATH = `${ESCROW_BASE_URL}${ESCROW_GET_DURATION}`;
 
 export interface GetEscrowResponse {
   escrowDays: number;
