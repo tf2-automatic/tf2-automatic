4f55878008157a09f25f982e9cae199d2ed79ae0
Nicklason <Nicklason@users.noreply.github.com>
2024-05-22 22:11:36 +0200

    feat(bot): publish account limitations
    Fix #332

--- libs/bot-data/src/lib/steam.ts
+++ libs/bot-data/src/lib/steam.ts
@@ -5,11 +5,23 @@ export const STEAM_EVENT_PREFIX = 'steam';
 
 export type SteamConnectedEventType = 'steam.connected';
 export type SteamDisconnectedEventType = 'steam.disconnected';
+export type SteamLimitationsEventType = 'steam.limitations';
 
 export const STEAM_CONNECTED_EVENT: SteamConnectedEventType = `${STEAM_EVENT_PREFIX}.connected`;
 export const STEAM_DISCONNECTED_EVENT: SteamDisconnectedEventType = `${STEAM_EVENT_PREFIX}.disconnected`;
+export const STEAM_LIMITATIONS_EVENT: SteamLimitationsEventType = `${STEAM_EVENT_PREFIX}.limitations`;
 
 export type SteamConnectedEvent = BaseEvent<SteamConnectedEventType>;
+
+export type SteamLimitationsEvent = BaseEvent<
+  SteamLimitationsEventType,
+  {
+    limited: boolean;
+    communityBanned: boolean;
+    locked: boolean;
+    canInviteFriends: boolean;
+  }
+>;
 
 export type SteamDisconnectedEvent = BaseEvent<
   SteamDisconnectedEventType,
