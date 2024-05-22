b8ba1533250b8c83d98589d79f8089e297ea1dfe
Nicklas <Nicklason@users.noreply.github.com>
2024-05-22 22:12:22 +0200

    feat(bot): add websession api (#359)
    Get the websession cookies
    
    Fix #356

--- libs/bot-data/src/lib/bot.ts
+++ libs/bot-data/src/lib/bot.ts
@@ -2,7 +2,10 @@ import { BaseEvent } from './events';
 
 export const BOT_BASE_URL = '/bot';
 export const BOT_PATH = `/`;
+export const BOT_WEBSESSION_PATH = '/websession';
+
 export const BOT_FULL_PATH = `${BOT_BASE_URL}`;
+export const BOT_WEBSESSION_FULL_PATH = `${BOT_BASE_URL}${BOT_WEBSESSION_PATH}`;
 
 export type BotReadyEventType = 'bot.ready';
 
@@ -13,4 +16,8 @@ export type BotReadyEvent = BaseEvent<BotReadyEventType>;
 export interface Bot {
   steamid64: string;
   apiKey: string;
+}
+
+export interface BotWebSession {
+  cookies: string[];
 }
