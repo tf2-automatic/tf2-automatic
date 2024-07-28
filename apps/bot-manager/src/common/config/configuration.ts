95bf257dc3a7b476881ebfe04c1ec1a6418d0a96
Nicklason <Nicklason@users.noreply.github.com>
2024-07-28 22:12:09 +0200

    fix(bot-manager): increase default leader timeouts

--- apps/bot-manager/src/common/config/configuration.ts
+++ apps/bot-manager/src/common/config/configuration.ts
@@ -2,13 +2,20 @@ import {
   EventsConfig,
   Redis,
   getEnv,
+  getEnvWithDefault,
   getEventsConfig,
 } from '@tf2-automatic/config';
+
+export interface RelayConfig {
+  leaderTimeout: number;
+  leaderRenewTimeout: number;
+}
 
 export interface Config {
   port: number;
   redis: Redis.Config;
   events: EventsConfig;
+  relay: RelayConfig;
 }
 
 export default (): Config => {
@@ -16,5 +23,13 @@ export default (): Config => {
     port: getEnv('PORT', 'integer')!,
     redis: Redis.getConfig(),
     events: getEventsConfig(),
+    relay: {
+      leaderTimeout: getEnvWithDefault('LEADER_TIMEOUT', 'integer', 5000),
+      leaderRenewTimeout: getEnvWithDefault(
+        'LEADER_RENEW_TIMEOUT',
+        'integer',
+        10000,
+      ),
+    },
   };
 };
