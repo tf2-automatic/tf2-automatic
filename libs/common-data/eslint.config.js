31420a12b97bbb69b0531b730710e132a9b5958d
Nicklas <Nicklason@users.noreply.github.com>
2025-03-30 19:01:16 +0200

    chore(common-data): extend publish eslint config

--- libs/common-data/eslint.config.js
+++ libs/common-data/eslint.config.js
@@ -1,19 +1,28 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
 const baseConfig = require('../../eslint.config.js');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
 
 module.exports = [
   ...baseConfig,
+  ...compat.extends('../../.eslintrc.publish.json'),
   {
-    files: ['**/*.json'],
-    rules: {
-      '@nx/dependency-checks': [
-        'error',
-        {
-          ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'],
-        },
-      ],
-    },
-    languageOptions: {
-      parser: require('jsonc-eslint-parser'),
-    },
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    // Override or add rules here
+    rules: {},
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
   },
 ];
